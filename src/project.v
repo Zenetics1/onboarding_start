53c76421e75a9cade48879b8536febf25701f22b
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2026-01-17 02:55:17 -0500

    fixed syntax

--- src/project.v
+++ src/project.v
@@ -39,12 +39,12 @@ module tt_um_uwasic_onboarding_William_Kim (
 
   pwm_peripheral pwm_peripheral_inst (
       .clk(clk),
-      .rst(rst_n),
+      .rst_n(rst_n),
       .en_reg_out_7_0(en_reg_out_7_0),
       .en_reg_out_15_8(en_reg_out_15_8),
       .en_reg_pwm_7_0(en_reg_pwm_7_0),
       .en_reg_pwm_15_8(en_reg_pwm_15_8),
-      .pwn_duty_cycle(pwm_duty_cycle),
+      .pwm_duty_cycle(pwm_duty_cycle),
       .out({uio_out, uo_out})
     );
 
