eb9f58ae2b2bb17cf97aca6145e1b8198101fba2
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2026-01-17 02:42:33 -0500

    fix project.v attempt 2

--- src/project.v
+++ src/project.v
@@ -34,8 +34,7 @@ module tt_um_uwasic_onboarding_William_Kim (
       .EN_OUT_15_8(en_reg_out_15_8),
       .EN_PWM_MODE_7_0(en_reg_pwm_7_0),
       .EN_PWM_MODE_15_8(en_reg_pwm_15_8),
-      .PWM_DUTY_CYCLE_7_0(pwn_duty_cycle),
-
+      .PWM_DUTY_CYCLE_7_0(pwn_duty_cycle)
     )
 
     pwm_peripheral pwm_peripheral_inst (
