8128778a3a392cac5ad9faddd0c8c9c4a15b9523
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2026-01-17 02:22:00 -0500

    fix attempt

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -17,6 +17,10 @@ module spi_peripheral(
 );
     
     reg transaction_ready;
+      //Transaction Components
+    wire RW_BIT; //Read/Write bit
+    wire [6:0] ADDR; //Address of register
+    wire [7:0] DATA; //Serial Data
 
     //Transaction Register Selector
     always @(posedge clk or negedge rst_n) begin
@@ -27,7 +31,7 @@ module spi_peripheral(
             EN_PWM_MODE_15_8 <= 8'h00;
             PWM_DUTY_CYCLE_7_0 <= 8'h00;
         end else begin
-            if(transaction_ready && RW_BIT == 1) begin
+            if(transaction_ready && RW_BIT == 1'b1) begin
                 case (ADDR)
                     7'h00: EN_OUT_7_0 <= DATA; 
                     7'h01: EN_OUT_15_8 <= DATA;
@@ -36,7 +40,6 @@ module spi_peripheral(
                     7'h04: PWM_DUTY_CYCLE_7_0 <= DATA;
                     default: ;
                 endcase
-            
             end
         end
     end
@@ -72,23 +75,20 @@ module spi_peripheral(
     assign SCLK_rising_edge = SCLK_sync1 & ~SCLK_sync2;
     assign nCS_rising_edge = nCS_sync1 & ~nCS_sync2;
     assign nCS_falling_edge = ~nCS_sync1 & nCS_sync2;
-
-
-    reg [3:0] counter;
+    
+    reg [4:0] counter;
     //Cycle Counter
     always @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             transaction_ready <= 1'b0;
         end else begin
-            if(counter == 15 && nCS_rising_edge) begin
+            if(counter == 16 && nCS_rising_edge) begin
                 transaction_ready <= 1'b1;
             end else begin
                 transaction_ready <= 1'b0;
             end
         end
     end
-
-    reg [15:0] shift_reg;
 
     //Shift Register Logic    
     always @(posedge clk or negedge rst_n) begin
@@ -99,17 +99,14 @@ module spi_peripheral(
             if(nCS_falling_edge) begin
                 shift_reg <= 16'b0;
                 counter <= 4'b0;
-        end else if(!nCS_sync2 && SCLK_rising_edge) begin
+            end else if(!nCS_sync2 && SCLK_rising_edge) begin
                 shift_reg <= {shift_reg[14:0], COPI_sync2};
                 counter <= counter + 4'd1; 
             end
         end
     end
 
-      //Transaction Components
-    wire RW_BIT; //Read/Write bit
-    wire [6:0] ADDR; //Address of register
-    wire [7:0] DATA; //Serial Data
+    reg [15:0] shift_reg;
 
     assign RW_BIT = shift_reg[15];
     assign ADDR = shift_reg[14:8];
