71f678431636767cbca1eb5683e7d16d5d2c3717
Zenetics1 <139074231+Zenetics1@users.noreply.github.com>
2026-01-18 16:29:36 -0500

    imports fix

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ from cocotb.triggers import ClockCycles
 from cocotb.types import Logic
 from cocotb.types import LogicArray
 from cocotb.utils import get_sim_time
-from cocotb.triggers import with_timeout
+from cocotb.result import with_timeout
 
 async def await_half_sclk(dut):
     """Wait for the SCLK signal to go high or low."""
@@ -180,13 +180,13 @@ async def test_pwm_freq(dut):
     try:
         dut._log.info("Wait for PWM out Rising Edge #1")
         await with_timeout(RisingEdge(dut.signal_out_bit0), 1, 'ms')
-        t_rising_edge1 = get_sim_time(unit='ns')
+        t_rising_edge1 = get_sim_time(units='ns')
 
         await ClockCycles(dut.clk, 5)
 
         dut._log.info("Wait for PWM out Rising Edge #2")
         await with_timeout(RisingEdge(dut.signal_out_bit0), 1, 'ms')
-        t_rising_edge2 = get_sim_time(unit='ns')
+        t_rising_edge2 = get_sim_time(units='ns')
     except cocotb.triggers.SimTimeoutError:
         assert False,f"Timeout Error occured: Signal is stuck"
 
@@ -242,15 +242,15 @@ async def test_pwm_duty(dut):
             try:
                 dut._log.info("Wait for PWM out Rising Edge #1")
                 await with_timeout(RisingEdge(dut.signal_out_bit0), 2, 'ms')
-                t_rising_edge1 = get_sim_time(unit='ns')
+                t_rising_edge1 = get_sim_time(units='ns')
 
                 dut._log.info("Wait for PWM out Falling Edge")
                 await with_timeout(FallingEdge(dut.signal_out_bit0), 2, 'ms')
-                t_falling_edge = get_sim_time(unit='ns')
+                t_falling_edge = get_sim_time(units='ns')
 
                 dut._log.info("Wait for PWM out Rising Edge #2")
                 await with_timeout(RisingEdge(dut.signal_out_bit0), 2, 'ms')
-                t_rising_edge2 = get_sim_time(unit='ns')
+                t_rising_edge2 = get_sim_time(units='ns')
             except cocotb.triggers.SimTimeoutError:
                 assert False,f"Timeout Error occured: Signal is stuck at {transaction}"
 
